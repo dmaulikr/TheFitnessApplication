1c4133d037564db97f1fa513fced6494104f5169
Milan <milanderuiter@gmail.com>
2017-08-17 09:27:26 +0200

    Remove unused code / breakpoints

--- SwiftOverload/Application/AppDelegate.swift
+++ SwiftOverload/Application/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+ import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         
         applicationLoadStart()
-        
+        applicationRequestNotifications();
         
         Utils.copyFile(fileName: "SwiftOverload.sql")
         applicationSetNavigation(color: .customDarkGray, textColor: .white)
@@ -32,6 +33,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
+        applicationSendReminder()
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
@@ -68,6 +70,48 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
         else {
             window?.rootViewController = storyboard.instantiateViewController(withIdentifier: "register")
+        }
+    }
+    
+    private func applicationRequestNotifications() {
+        
+        
+        if #available(iOS 10, *) {
+            let options: UNAuthorizationOptions = [.alert, .sound];
+            let center = UNUserNotificationCenter.current()
+            center.requestAuthorization(options: options) {
+                (granted, error) in
+            }
+            
+            // Remove all notifications
+            
+            center.removeAllPendingNotificationRequests()
+ 
+        }
+
+    }
+    
+    private func applicationSendReminder() {
+        let center = UNUserNotificationCenter.current()
+        center.getNotificationSettings { (settings) in
+            if settings.authorizationStatus == .authorized {
+                let content = UNMutableNotificationContent()
+                content.body = "It has been a while, please come back!"
+                content.sound = UNNotificationSound.default()
+                                
+                let calendar:Calendar = Calendar(identifier: .gregorian)
+                let tomorrow = Date().addingTimeInterval(172800)
+                var components = calendar.dateComponents([.year, .month, .day, .hour, .minute], from: tomorrow)
+                components.hour = 20
+                components.minute = 0
+                
+                let trigger = UNCalendarNotificationTrigger(dateMatching: components, repeats: false)
+                
+                let identifier = "customNotification"
+                let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
+                
+                center.add(request, withCompletionHandler: nil)
+            }
         }
     }
 
